8132b1528d5686f2f7cc87bad92223f74d10996a
mjmott <mjmott@umich.edu>
2018-10-13 06:39:16 -0400

    Finished both hold key functions

--- key_input.py
+++ key_input.py
@@ -1,7 +1,6 @@
 import time
 import pyautogui
 
-
 def hold_Key (hold_time, key_string):
     start = time.time()
     while time.time() - start < hold_time:
@@ -15,16 +14,3 @@ def hold_Two_Keys (hold_time, key_string1, key_string2):
         pyautogui.keyDown(key_string2)
     pyautogui.keyUp(key_string1)
     pyautogui.keyUp(key_string2)
-
-pyautogui.click()
-
-pyautogui.keyDown('alt')
-pyautogui.keyUp('alt')
-
-pyautogui.keyDown('right')
-time.sleep(1)
-pyautogui.keyUp('right')
-
-hold_Two_Keys(1, 'right', 'alt')
-
-
