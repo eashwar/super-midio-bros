dced539384079894b2a5f3c29496aead4b4408c8
Eashwar Mohan <luigi629@gmail.com>
2018-10-13 21:32:02 -0400

    display keyboard notes to tell user what notes to play

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import sys
 import os
 import time
+import math
 import pyautogui
 import pygame
 import pygame.midi
@@ -64,7 +65,71 @@ t = threading.Thread(target=emu_controller)
 t.daemon = True
 t.start()
 
+disp_width = 1261
+disp_height = 169
+
+keyboardDisp = pygame.display.set_mode((disp_width, disp_height))
+pygame.display.set_caption("Super MIDIo Bros.")
+keyboardImg = pygame.image.load("res/keyboard.png")
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+cyan = (0, 255, 255)
+octave_width = math.trunc((disp_width - 34) / 5)
+white_key_width = math.trunc(octave_width / 7)
+rect_width = 21
+rect_height = 40
+rect_black_key_top = 60
+rect_white_key_top = 120
+
+semitone_to_scale_degree = {
+    0: 0,
+    2: 1,
+    4: 2,
+    5: 3,
+    7: 4,
+    9: 5,
+    11: 6,
+}
+
+def get_octave(note):
+    return math.trunc((note - 36) / 12)
+
+def is_black_key(note):
+    return ((note % 12) in [1, 3, 6, 8, 10])
+
+def get_rect(note):
+    if is_black_key(note):
+        rect_left = (octave_width * get_octave(note)) + 25 + semitone_to_scale_degree[(note % 12) - 1] * white_key_width
+        rect_top = rect_black_key_top
+    else:
+        rect_left = (octave_width * get_octave(note)) + 6 + semitone_to_scale_degree[note % 12] * white_key_width
+        rect_top = rect_white_key_top
+    return Rect(rect_left, rect_top, rect_width, rect_height)
+
+def draw_sfx_rects():
+    pygame.draw.rect(keyboardDisp, green, get_rect(sequences.jump[0]))
+    pygame.draw.rect(keyboardDisp, green, get_rect(sequences.jump[1]))
+    pygame.draw.rect(keyboardDisp, blue, get_rect(sequences.reverse[0]))
+    rev_2_rect = get_rect(sequences.reverse[1])
+    rev_2_rect.height /= 2
+    pygame.draw.rect(keyboardDisp, blue, rev_2_rect)
+    pause_1_rect = get_rect(sequences.pause[0])
+    pause_1_rect.height /= 2
+    pause_1_rect.top += pause_1_rect.height
+    pygame.draw.rect(keyboardDisp, cyan, pause_1_rect)
+    pygame.draw.rect(keyboardDisp, cyan, get_rect(sequences.pause[1]))
+
+clock = pygame.time.Clock()
+
 while True:
+    keyboardDisp.blit(keyboardImg, (0, 0))
+    draw_sfx_rects()
+    pygame.draw.rect(keyboardDisp, red, get_rect(sequences.melody1_1[melodyPosition]))        
+    pygame.display.flip()
+    clock.tick(60)
+
     currentNote = inputs.getInput()
     if currentNote == sequences.melody1_1[melodyPosition]:
         if goingRight:
